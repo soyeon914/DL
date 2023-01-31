479e624e06d1f2c5f0ee004ea21edf338fff2d3b
thdus5918647 <thdus5918647@naver.com>
2023-01-31 18:07:17 +0900

    commit

---
+++ Federated_Learning/client1.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+            loss= 'sparse_categorical_crossentropy',
+            metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+loss, accuracy = model.evaluate(x_test, y_test)
+
+import flwr as fl
+
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return model.get_weights()
+
+    def fit(self, parameters, config):
+        model.set_weights(parameters)
+        model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
+        return model.get_weights(), len(x_train), {}
+
+    def evaluate(self, parameters, config):
+        model.set_weights(parameters)
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, len(x_test), {"accuracy": float(accuracy)}
+
+fl.client.start_numpy_client(server_address="[::]:8080", client=CifarClient())
